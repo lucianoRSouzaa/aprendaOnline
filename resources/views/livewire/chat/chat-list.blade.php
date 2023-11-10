9df6f463f16e765fa58025224dc8108bf885d825
Luciano Souza <lucianorodriguess101@gmail.com>
2023-11-09 21:25:42 -0300

    Created component chat-list

---
+++ resources/views/livewire/chat/chat-list.blade.php
@@ -0,0 +1,34 @@
+<section class="col-4 conversas barra">
+    <div class="pesquisar d-flex align-items-center justify-content-between">
+        <div class="pesquisa">
+            <div class="searchbar d-flex align-items-center">
+                <i class="fa fa-search" aria-hidden="true"></i>
+                <input type="text" placeholder="Pesquisar...">
+            </div>
+        </div>
+        <div class="adicionar d-flex justify-content-center">
+            <i class="fa-solid fa-plus"></i>
+        </div>
+    </div>
+    <div class="box-conversa d-flex active">
+        <img class="photo" src="{{ asset('images/defaultUser.jpg') }}"></img>
+        <div class="desc-contact d-flex align-items-center">
+            <p class="name">Luciano</p>
+        </div>
+        <span class="notification d-flex justify-content-center align-items-center">2</span>
+    </div>
+    <div class="box-conversa d-flex">
+        <img class="photo" src="{{ asset('images/defaultUser.jpg') }}"></img>
+        <div class="desc-contact d-flex align-items-center">
+            <p class="name">Gustavo</p>
+        </div>
+        <span class="notification d-flex justify-content-center align-items-center">3</span>
+    </div>
+    <div class="box-conversa d-flex">
+        <img class="photo" src="{{ asset('images/defaultUser.jpg') }}"></img>
+        <div class="desc-contact d-flex align-items-center">
+            <p class="name">Leandro</p>
+        </div>
+        <span class="notification d-flex justify-content-center align-items-center">1</span>
+    </div>
+</section>
